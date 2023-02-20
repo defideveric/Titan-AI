5aea016b02b768043ca9b25566cf152e93539922
erichudson <defideveric@gmail.com>
2023-02-20 09:35:29 -0500

    first commit

--- client/script.js
+++ client/script.js
@@ -21,21 +21,19 @@ function loader(element) {
 }
 
 function typeText(element, text) {
-    let index = 0
+    let index = 0;
 
     let interval = setInterval(() => {
         if (index < text.length) {
-            element.innerHTML += text.charAt(index)
-            index++
+            element.innerHTML += text.charAt(index);
+            index++;
         } else {
             clearInterval(interval)
         }
     }, 20)
 }
 
-// generate unique ID for each message div of bot
-// necessary for typing text effect for that specific reply
-// without unique ID, typing text will work on every element
+
 function generateUniqueId() {
     const timestamp = Date.now();
     const randomNumber = Math.random();
@@ -46,7 +44,7 @@ function generateUniqueId() {
 
 function chatStripe(isAi, value, uniqueId) {
     return (
-        `
+      `
         <div class="wrapper ${isAi && 'ai'}">
             <div class="chat">
                 <div class="profile">
@@ -58,61 +56,57 @@ function chatStripe(isAi, value, uniqueId) {
                 <div class="message" id=${uniqueId}>${value}</div>
             </div>
         </div>
-    `
+      `
     )
 }
 
 const handleSubmit = async (e) => {
-    e.preventDefault()
+  e.preventDefault();
 
-    const data = new FormData(form)
+  const data = new FormData(form);
 
-    // user's chatstripe
-    chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+  // user's chatstripe
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
 
-    // to clear the textarea input 
-    form.reset()
+  // to clear the textarea input 
+  form.reset();
 
-    // bot's chatstripe
-    const uniqueId = generateUniqueId()
-    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+  // bot's chatstripe
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
 
-    // to focus scroll to the bottom 
-    chatContainer.scrollTop = chatContainer.scrollHeight;
+  // to focus scroll to the bottom 
+  chatContainer.scrollTop = chatContainer.scrollHeight;
 
-    // specific message div 
-    const messageDiv = document.getElementById(uniqueId)
+  // specific message div 
+  const messageDiv = document.getElementById(uniqueId);
 
-    // messageDiv.innerHTML = "..."
-    loader(messageDiv)
+  // messageDiv.innerHTML = "..."
+  loader(messageDiv);
 
-    const response = await fetch('http://localhost:5000', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            prompt: data.get('prompt')
-        })
-    })
+  //fetch data from server
+  const response = await fetch('http://localhost:5000', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json',},
+    body: JSON.stringify({prompt: data.get('prompt')})});
 
-    clearInterval(loadInterval)
-    messageDiv.innerHTML = " "
+    clearInterval(loadInterval);
+    messageDiv.innerHTML = " ";
 
     if (response.ok) {
-        const data = await response.json();
-        const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
+      const data = await response.json();
+      const parsedData = data.bot.trim();
 
-        typeText(messageDiv, parsedData)
+      typeText(messageDiv, parsedData);
     } else {
-        const err = await response.text()
+      const err = await response.text();
 
-        messageDiv.innerHTML = "Something went wrong"
-        alert(err)
+      messageDiv.innerHTML = "Something went wrong"
+      alert(err)
     }
 }
 
-form.addEventListener('submit', handleSubmit)
+form.addEventListener('submit', handleSubmit);
 form.addEventListener('keyup', (e) => {
     if (e.keyCode === 13) {
         handleSubmit(e)
